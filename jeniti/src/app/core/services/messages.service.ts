3bd3b623330389496f2fa89294525641ff6d65f3
AlexTraveylan <monsieurdemares@gmail.com>
2023-01-07 14:03:49 +0100

    messages Service

--- jeniti/src/app/core/services/messages.service.ts
+++ jeniti/src/app/core/services/messages.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { env } from 'src/app/environ/env';
+import { Message } from '../models/messages';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessagesService {
+  public messages$!: Observable<Message[]>;
+  public bMessages$!: BehaviorSubject<Message[]>;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.messages$ = this.http.get<Message[]>(env.urlMessages);
+  }
+
+  refresh(): void {
+    this.http.get<Message[]>(env.urlMessages).subscribe(this.bMessages$.next);
+  }
+
+  deleteMessagebyId(MessageId: number) {
+    return this.http
+      .delete(`${env.urlMessages}/${MessageId}`)
+      .subscribe(this.refresh);
+  }
+
+  addMessage(Message: Message): Observable<Message> {
+    return this.http
+      .post<Message>(env.urlMessages, Message)
+      .pipe(tap(this.refresh));
+  }
+
+  updateMessage(Message: Message): Observable<Message> {
+    return this.http
+      .put<Message>(env.urlMessages, Message)
+      .pipe(tap(this.refresh));
+  }
 }
