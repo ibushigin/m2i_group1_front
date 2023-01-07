1cf05416eb24385fbd8f81eb28a27b3ccdfcf57a
AlexTraveylan <monsieurdemares@gmail.com>
2023-01-07 14:54:47 +0100

    add main, login, register modules

--- jeniti/src/app/core/core.module.ts
+++ jeniti/src/app/core/core.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { LoginModule } from '../login/login.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [LoginModule],
 })
-export class CoreModule { }
+export class CoreModule {}
