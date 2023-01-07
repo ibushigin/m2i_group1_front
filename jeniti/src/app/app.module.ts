8d0dd9a391b1d0b05da053eae0b5dfc381b6b62a
AlexTraveylan <monsieurdemares@gmail.com>
2023-01-07 16:11:56 +0100

    main ui

--- jeniti/src/app/app.module.ts
+++ jeniti/src/app/app.module.ts
@@ -5,16 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { MainHeaderComponent } from './main/main-header/main-header.component';
-import { MainMessagesContainerComponent } from './main/main-messages-container/main-messages-container.component';
-import { MainMessagesFormComponent } from './main/main-messages-form/main-messages-form.component';
-import { MainNavHeaderComponent } from './main/main-nav-header/main-nav-header.component';
-import { MainNavComponent } from './main/main-nav/main-nav.component';
-import { MainNavCardComponent } from './main/main-nav-card/main-nav-card.component';
+import { MainModule } from './main/main.module';
 
 @NgModule({
-  declarations: [AppComponent, MainHeaderComponent, MainMessagesContainerComponent, MainMessagesFormComponent, MainNavHeaderComponent, MainNavComponent, MainNavCardComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CoreModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CoreModule,
+    MainModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- jeniti/src/app/main/main.module.ts
+++ jeniti/src/app/main/main.module.ts
@@ -1,12 +1,30 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainMessagesContainerComponent } from './main-messages-container/main-messages-container.component';
+import { MainMessagesFormComponent } from './main-messages-form/main-messages-form.component';
+import { MainNavHeaderComponent } from './main-nav-header/main-nav-header.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { MainUiComponent } from './main-ui/main-ui.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    MainNavHeaderComponent,
+    MainNavComponent,
+    MainUiComponent,
+    MainMessagesFormComponent,
+    MainMessagesContainerComponent,
+    MainHeaderComponent,
+  ],
+  imports: [CommonModule, SharedModule],
+  exports: [
+    MainNavHeaderComponent,
+    MainNavComponent,
+    MainUiComponent,
+    MainMessagesFormComponent,
+    MainMessagesContainerComponent,
+    MainHeaderComponent,
+  ],
 })
-export class MainModule { }
+export class MainModule {}
