ac5189b1f9b2a6e44d3807b5c233b9b30aed77a6
Jean Lalanne Desktop <jexn.lxlxnne@gmail.com>
2023-01-07 16:09:44 +0100

    style

--- jeniti/src/app/app.module.ts
+++ jeniti/src/app/app.module.ts
@@ -5,15 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { MainHeaderComponent } from './main/main-header/main-header.component';
-import { MainMessagesContainerComponent } from './main/main-messages-container/main-messages-container.component';
-import { MainMessagesFormComponent } from './main/main-messages-form/main-messages-form.component';
-import { MainNavHeaderComponent } from './main/main-nav-header/main-nav-header.component';
-import { MainNavComponent } from './main/main-nav/main-nav.component';
-import { MainNavCardComponent } from './main/main-nav-card/main-nav-card.component';
+
 
 @NgModule({
-  declarations: [AppComponent, MainHeaderComponent, MainMessagesContainerComponent, MainMessagesFormComponent, MainNavHeaderComponent, MainNavComponent, MainNavCardComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
--- jeniti/src/app/login/login.module.ts
+++ jeniti/src/app/login/login.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {PageLoginComponent} from "./page-login/page-login.component";
+import { PageRegisterComponent } from './page-register/page-register.component';
+import { PageResetComponent } from './page-reset/page-reset.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PageLoginComponent, PageRegisterComponent, PageResetComponent],
   imports: [
     CommonModule
   ]
