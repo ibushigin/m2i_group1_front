858b66158bf8c62bd57f2c70c29cfe79251c7d52
AlexTraveylan <monsieurdemares@gmail.com>
2023-01-08 15:19:16 +0100

    fix redirect problem with btn-component

--- jeniti/src/app/shared/components/btn/btn.component.ts
+++ jeniti/src/app/shared/components/btn/btn.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-btn',
@@ -7,4 +8,10 @@ import { Component, Input } from '@angular/core';
 })
 export class BtnComponent {
   @Input() route!: string;
+
+  constructor(private router: Router) {}
+
+  goTo() {
+    this.router.navigate([this.route]);
+  }
 }
