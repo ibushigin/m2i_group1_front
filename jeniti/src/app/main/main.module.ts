bd087cb8fa62a6e6bf49cf10b8ace0a0a8af6ead
AlexTraveylan <monsieurdemares@gmail.com>
2023-01-08 10:40:47 +0100

    fix last commit

--- jeniti/src/app/main/main.module.ts
+++ jeniti/src/app/main/main.module.ts
@@ -4,25 +4,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ChannelsModule } from '../channels/channels.module';
 import { MessagesModule } from '../messages/messages.module';
 import { SharedModule } from '../shared/shared.module';
-import { MainGabaritComponent } from './structure/main-gabarit/main-gabarit.component';
-import { MainHeaderComponent } from './structure/main-header/main-header.component';
-import { MainMessagesContainerComponent } from './structure/main-messages-container/main-messages-container.component';
-import { MainMessagesFormComponent } from './structure/main-messages-form/main-messages-form.component';
-import { MainNavHeaderComponent } from './main-nav-header/main-nav-header.component';
-import { MainNavComponent } from './structure/main-nav/main-nav.component';
 import { MainRoutingModule } from './main-routing.module';
 import { AddChannelComponent } from './pages/add-channel/add-channel.component';
 import { EditChannelComponent } from './pages/edit-channel/edit-channel.component';
 import { EditMessageComponent } from './pages/edit-message/edit-message.component';
 import { GetMessagesComponent } from './pages/get-messages/get-messages.component';
+import { MainGabaritComponent } from './structure/main-gabarit/main-gabarit.component';
+import { MainHeaderComponent } from './structure/main-header/main-header.component';
+import { MainMessagesContainerComponent } from './structure/main-messages-container/main-messages-container.component';
+import { MainMessagesFormComponent } from './structure/main-messages-form/main-messages-form.component';
+import { MainNavHeaderComponent } from './structure/main-nav-header/main-nav-header.component';
+import { MainNavComponent } from './structure/main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
-    MainNavHeaderComponent,
-    MainNavComponent,
-    MainMessagesFormComponent,
-    MainMessagesContainerComponent,
     MainHeaderComponent,
+    MainMessagesContainerComponent,
+    MainMessagesFormComponent,
+    MainNavComponent,
+    MainNavHeaderComponent,
     MainGabaritComponent,
     AddChannelComponent,
     EditChannelComponent,
@@ -39,10 +39,10 @@ import { GetMessagesComponent } from './pages/get-messages/get-messages.componen
     FormsModule,
   ],
   exports: [
-    GetMessagesComponent,
+    AddChannelComponent,
     EditChannelComponent,
     EditMessageComponent,
-    AddChannelComponent,
+    GetMessagesComponent,
   ],
 })
 export class MainModule {}
