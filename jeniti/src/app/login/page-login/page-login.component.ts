f1a8afce1dae95b89136eb66707609969dc2a58a
AlexTraveylan <monsieurdemares@gmail.com>
2023-01-09 14:06:04 +0100

    form login

--- jeniti/src/app/login/page-login/page-login.component.ts
+++ jeniti/src/app/login/page-login/page-login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-login',
   templateUrl: './page-login.component.html',
-  styleUrls: ['./page-login.component.scss']
+  styleUrls: ['./page-login.component.scss'],
 })
 export class PageLoginComponent {
+  loginForm!: FormGroup;
+  wrongCredentials: boolean = false;
 
+  constructor(private formBuilder: FormBuilder, private route: Router) {
+    this.loginForm = this.formBuilder.group({
+      email: [null, Validators.required],
+      password: [null, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    console.log('logged');
+  }
 }
