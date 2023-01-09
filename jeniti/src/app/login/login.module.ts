382b71e585e7c3c15af4efd65e77a9938e77f9a9
AlexTraveylan <monsieurdemares@gmail.com>
2023-01-09 16:06:50 +0100

    logout

--- jeniti/src/app/login/login.module.ts
+++ jeniti/src/app/login/login.module.ts
@@ -5,9 +5,10 @@ import { LoginRoutingModule } from './login-routing.module';
 import { PageLoginComponent } from './page-login/page-login.component';
 import { PageRegisterComponent } from './page-register/page-register.component';
 import { PageResetComponent } from './page-reset/page-reset.component';
+import { PageLogoutComponent } from './page-logout/page-logout.component';
 
 @NgModule({
-  declarations: [PageLoginComponent, PageRegisterComponent, PageResetComponent],
+  declarations: [PageLoginComponent, PageRegisterComponent, PageResetComponent, PageLogoutComponent],
   imports: [CommonModule, LoginRoutingModule, ReactiveFormsModule],
 })
 export class LoginModule {}
