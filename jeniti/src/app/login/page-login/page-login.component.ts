8e72305ee7c5800a04857d884a297dc6115b42b1
AlexTraveylan <monsieurdemares@gmail.com>
2023-01-09 15:57:59 +0100

    login begin

--- jeniti/src/app/login/page-login/page-login.component.ts
+++ jeniti/src/app/login/page-login/page-login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-page-login',
@@ -11,7 +12,11 @@ export class PageLoginComponent {
   loginForm!: FormGroup;
   wrongCredentials: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private route: Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: Router,
+    private auth: AuthService
+  ) {
     this.loginForm = this.formBuilder.group({
       email: [null, Validators.required],
       password: [null, Validators.required],
@@ -19,6 +24,18 @@ export class PageLoginComponent {
   }
 
   onSubmit() {
-    console.log('logged');
+    this.auth.login(this.loginForm.value).subscribe({
+      next: (data) => {
+        if (data.isLogged) {
+          localStorage.setItem('currentUser', JSON.stringify(data));
+          this.route.navigate(['/main']);
+        } else {
+          console.log('TU NE PASSES PAS');
+        }
+      },
+      error: (error) => {
+        console.log('Pas connecté degage');
+      },
+    });
   }
 }
