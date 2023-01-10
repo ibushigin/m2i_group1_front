cccceac152306d5cd55a3fb3b48e4992baaebf8e
Jean Lalanne Desktop <jexn.lxlxnne@gmail.com>
2023-01-10 16:17:43 +0100

    edit message

--- jeniti/src/app/messages/messages-bubble/messages-bubble.component.ts
+++ jeniti/src/app/messages/messages-bubble/messages-bubble.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject, Observable, tap} from 'rxjs';
 import { Message } from 'src/app/core/models/messages';
 import { User } from 'src/app/core/models/users';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { MessagesService } from 'src/app/core/services/messages.service';
+
 
 @Component({
   selector: 'app-messages-bubble',
@@ -14,16 +15,44 @@ export class MessagesBubbleComponent implements OnInit {
   @Input() message!: Message;
   route!: string;
   sessionUser$!: BehaviorSubject<User>;
+  msgHidden: boolean = false;
+  editHidden: boolean = true;
+  contentInput!: string;
+  idMessage!: number;
+  message$!: Observable<Message>;
 
-  constructor(private mService: MessagesService, private auth: AuthService) {
+  constructor(
+    private mService: MessagesService,
+    private auth: AuthService) {
     this.sessionUser$ = this.auth.bSessionUser$;
   }
 
   ngOnInit(): void {
-    this.route = `/editMessage/${this.message.id}`;
+    this.idMessage = this.message.id;
+    this.message$ = this.mService
+      .getMessageById(this.idMessage)
+      .pipe(tap((data)=>
+        (this.contentInput = data.content)
+      ));
+
+
   }
 
   deleteMessage(idMessage: number): void {
     this.mService.deleteMessagebyId(idMessage);
+  }
+  hide(){
+    this.msgHidden = true;
+    this.editHidden = false;
+  }
+  onSubmit(){
+    this.msgHidden = false;
+    this.editHidden = true;
+    if(this.contentInput){
+      let newMessage: Message = new Message();
+      newMessage.id = this.idMessage;
+      newMessage.content = this.contentInput;
+      this.mService.updateMessage(newMessage).subscribe();
+    }
   }
 }
--- jeniti/src/app/messages/messages.module.ts
+++ jeniti/src/app/messages/messages.module.ts
@@ -5,10 +5,11 @@ import { SharedModule } from '../shared/shared.module';
 import { MessagesBubbleComponent } from './messages-bubble/messages-bubble.component';
 import { MessagesListComponent } from './messages-list/messages-list.component';
 import { MessagesRoutingModule } from './messages-routing.module';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [MessagesListComponent, MessagesBubbleComponent],
-  imports: [CommonModule, MessagesRoutingModule, SharedModule],
+    imports: [CommonModule, MessagesRoutingModule, SharedModule, FormsModule],
   exports: [MessagesListComponent],
 })
 export class MessagesModule {}
