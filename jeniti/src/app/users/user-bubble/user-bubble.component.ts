0143f118b3e3556cd003f996a5feb157814f5420
Jean Lalanne Desktop <jexn.lxlxnne@gmail.com>
2023-01-11 20:44:49 +0100

    retouche css

--- jeniti/src/app/users/user-bubble/user-bubble.component.ts
+++ jeniti/src/app/users/user-bubble/user-bubble.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { User } from 'src/app/core/models/users';
 
 @Component({
